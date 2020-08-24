9f40cf1b31874a70e86c09291e028cde58e51150
Volkov Alexander <volkov.525@gmail.com>
2020-08-24 10:08:27 +0300

    make hint wrap

--- src/components/Hint.vue
+++ src/components/Hint.vue
@@ -1,14 +1,18 @@
 <template lang="pug">
 v-slide-y-transition(mode="out-in")
 	.hint(v-show="searchMode")
-		v-checkbox(dense label="Документы" v-model="doc" :disabled="sotr" :class="md ? 'my' : '' ")
-		v-checkbox(dense label="Задания" v-model="task" :disabled="sotr")
-		v-checkbox(dense label="Файлы" v-model="task" :disabled="sotr")
-		.vert
-		v-checkbox(dense label="Я - автор" :disabled="sotr")
-		v-checkbox(dense label="По организации" :disabled="sotr")
-		.vert(v-shodense w="!sotr")
-		v-checkbox(dense label="Точное совпадение" )
+		div.one
+			v-checkbox(dense label="Документы" v-model="doc" :disabled="sotr" :class="md ? 'my' : '' ")
+		div.two
+			v-checkbox(dense label="Задания" v-model="task" :disabled="sotr")
+		div.three
+			v-checkbox(dense label="Файлы" v-model="task" :disabled="sotr")
+		div.four
+			v-checkbox(dense label="Я - автор" :disabled="sotr")
+		div.five
+			v-checkbox(dense label="По организации" :disabled="sotr")
+		div.six
+			v-checkbox(dense label="Точное совпадение" )
 
 </template>
 
@@ -28,28 +32,48 @@ export default {
 <style scoped lang="scss">
 @import '@/assets/css/colors.scss';
 
+.one {
+	grid-column: 1/2;
+	grid-row: 1;
+}
+.two {
+	grid-column: 2/3;
+	@media screen and (max-width: 1260px) {
+		grid-column: 1/2;
+		grid-row: 2;
+	}
+}
+.three {
+	grid-column: 3/4;
+	@media screen and (max-width: 1260px) {
+		grid-column: 2/3;
+		grid-row: 1;
+	}
+}
+.four {
+	grid-column: 4/5;
+	@media screen and (max-width: 1260px) {
+		grid-column: 3/4;
+		grid-row: 2;
+	}
+}
 .hint {
 	background: #fff;
 	width: 100%;
-	padding: 0 2rem;
-	display: flex;
-	flex-wrap: wrap;
+	padding: .5rem 1rem;
+	display: grid;
+	grid-template-columns: repeat(auto-fit, minmax(165px, 1fr));
+	grid-template-rows: auto;
+	grid-gap: 0 .25rem;
 	border-bottom: 1px solid #B0BEC5;
 	.v-input--selection-controls {
 		margin: 0;
 		padding: 0;
-		margin-top: .9rem;
+		height: 24px;
 		margin-right: 2rem;
 		margin-bottom: 0;
-
+		white-space: nowrap;
 	}
-}
-.vert {
-	height: 26px;
-	width: 1px;
-	background: #ccc;
-	margin-right: 2rem;
-	margin-top: 11px;
 }
 
 </style>
