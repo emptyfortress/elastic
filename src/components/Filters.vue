8279597fd2c8be3bf00447d59f8c3d737d26d23e
Volkov Alexander <volkov.525@gmail.com>
2020-08-26 14:57:26 +0300

    before redone

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -5,9 +5,9 @@
 		.lin Сбросить все
 	section
 		.section(v-for="item in typecard") {{ item.section }}
-			.item(v-for="prop in item.child")
-				v-checkbox(dense :label="prop.name" :value="prop.id" v-model="checkedtype").my
-				.badge {{ badge(prop.id) }}
+			.item(v-for="prop in item.child" v-if="badge(item.id, prop.id)")
+				v-checkbox(dense :label="prop.name" :value="prop.id" v-model="checked").my
+				.badge {{ badge(item.id, prop.id) }}
 
 </template>
 
@@ -20,10 +20,11 @@ export default {
 			taskC: false,
 			fileC: false,
 			filter: [],
-			checkedtype: [],
+			checked: [],
 			typecard: [
 				{
-					section: 'Тип документа',
+					id: 0,
+					section: 'Тип карточки',
 					child: [
 						{ id: 1, name: 'Документ' },
 						{ id: 2, name: 'Задание' },
@@ -32,11 +33,13 @@ export default {
 					]
 				},
 				{
-					section: 'Вид карточки',
+					id: 1,
+					section: 'Вид документа',
 					child: [
-						{ id: 4, name: 'Документ' },
-						{ id: 6, name: 'Задание' },
-						{ id: 7, name: 'Файл' },
+						{ id: 5, name: 'Приказ' },
+						{ id: 6, name: 'Договор' },
+						{ id: 7, name: 'Служебная записка' },
+						{ id: 8, name: 'Заявление' },
 					]
 				},
 			]
@@ -52,29 +55,88 @@ export default {
 		},
 	},
 	methods: {
-		badge(e) {
+		badge(a, e) {
 			switch (e) {
 			case 1:
 				return this.searchItemsResults.filter( item => item.item.type === 'doc').length
 			case 2:
 				return this.searchItemsResults.filter( item => item.item.type === 'task').length
 			case 3:
+				return this.searchItemsResults.filter( item => item.item.typ === 'Группа заданий').length
+			case 4:
 				return this.searchItemsResults.filter( item => item.item.type === 'file').length
+			case 5:
+				return this.searchItemsResults.filter( item => item.item.vid === 'Приказ').length
+			case 6:
+				return this.searchItemsResults.filter( item => item.item.vid === 'Договор').length
+			case 7:
+				return this.searchItemsResults.filter( item => item.item.vid === 'Служебная записка').length
+			case 8:
+				return this.searchItemsResults.filter( item => item.item.vid === 'Заявление').length
 			}
 		}
 	},
 	watch: {
-		docC: function() {
-			if (this.docC) {
-				let temp = this.searchItemsResults.filter( item => item.item.type === 'doc')
-				this.$store.commit('setFilterResults', temp)
-			} else this.$store.commit('setFilterResults', this.filter)
-		},
-		taskC: function() {
-			if (this.taskC) {
-				let temp = this.searchItemsResults.filter( item => item.item.type === 'task')
-				this.$store.commit('setFilterResults', temp)
-			} else this.$store.commit('setFilterResults', this.filter)
+		checked: function() {
+			if (this.checked.length) {
+				if (this.checked.includes(1) && this.checked.includes(2)) {
+					let temp = this.searchItemsResults.filter( item => {
+						return (item.item.type === 'doc' || item.item.type === 'task')
+					})
+					this.$store.commit('setFilterResults', temp)
+				} 
+				else if (this.checked.includes(1)) {
+					let temp = this.searchItemsResults.filter( item => {
+						return (item.item.type === 'doc')
+					})
+					this.$store.commit('setFilterResults', temp)
+				}
+				else if (this.checked.includes(2)) {
+					let temp = this.searchItemsResults.filter( item => {
+						return (item.item.type === 'task')
+					})
+					this.$store.commit('setFilterResults', temp)
+				}
+				else if (this.checked.includes(3)) {
+					let temp = this.searchItemsResults.filter( item => {
+						return (item.item.typ === 'Группа заданий')
+					})
+					this.$store.commit('setFilterResults', temp)
+				}
+				else if (this.checked.includes(4)) {
+					let temp = this.searchItemsResults.filter( item => {
+						return (item.item.type === 'file')
+					})
+					this.$store.commit('setFilterResults', temp)
+				}
+				else if (this.checked.includes(5)) {
+					let temp = this.searchItemsResults.filter( item => {
+						return (item.item.vid === 'Приказ')
+					})
+					this.$store.commit('setFilterResults', temp)
+				}
+				else if (this.checked.includes(6)) {
+					let temp = this.searchItemsResults.filter( item => {
+						return (item.item.vid === 'Договор')
+					})
+					this.$store.commit('setFilterResults', temp)
+				}
+				else if (this.checked.includes(7)) {
+					let temp = this.searchItemsResults.filter( item => {
+						return (item.item.vid === 'Служебная записка')
+					})
+					this.$store.commit('setFilterResults', temp)
+				}
+				else if (this.checked.includes(8)) {
+					let temp = this.searchItemsResults.filter( item => {
+						return (item.item.vid === 'Заявление')
+					})
+					this.$store.commit('setFilterResults', temp)
+				}
+
+			} else {
+				this.$store.commit('setFilterResults', this. searchItemsResults)
+			}
 		}
 	}
 }
